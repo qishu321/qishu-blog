76c0fc474f3c87de60f2793799489f80c284f06e
郭梦飞 <v_guomengfei@bilibili.com>
2024-06-14 10:15:00 +0800

    0614

--- docs/.vuepress/config/themeConfig.js
+++ docs/.vuepress/config/themeConfig.js
@@ -41,7 +41,7 @@ module.exports = {
   },
   author: {
     // 文章默认的作者信息，可在md文件中单独配置此信息 String | {name: String, link: String}
-    name: "二丫讲梵", // 必需
+    name: "七书", // 必需
     link: "https://github.com/qishu321", // 可选的
   },
   social: {
