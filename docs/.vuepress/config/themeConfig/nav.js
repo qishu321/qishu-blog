6d3ee4ec4c1b118517bb1c84aa33d552ea99bdfc
qishu <1779009810@qq.com>
2025-07-18 10:47:51 +0800

    0718更新

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -1,10 +1,9 @@
 // nav
 module.exports = [
   { text: '首页', link: '/' },
-  { text: '运维',link: '/one/',},
-  { text: 'two',link: '/two/',},
-  { text: 'three', link: '/three/' },
+  { text: '运维',link: '/devops/',},
+  { text: '编程',link: '/backend/',},
+  { text: '更多', link: '/more/' },
   { text: '关于我', link: '/about/' },
   { text: '留言板', link: '/message-board/' },
-  // { text: '我的博客', link: 'https://qishu321.github.io/qishu-blog' },
 ]
