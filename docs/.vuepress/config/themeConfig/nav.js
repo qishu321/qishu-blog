10df490e1f71a8f9d3aad3b5b3b8186df0526be1
qishu <1779009810@qq.com>
2025-07-19 16:14:15 +0800

    优化

--- docs/.vuepress/config/themeConfig/nav.js
+++ docs/.vuepress/config/themeConfig/nav.js
@@ -5,17 +5,17 @@ module.exports = [
     text: '运维',
     link: '/devops/',
     items: [
+      { text: 'Docker相关', link: '/devops/docker/' },
+      { text: 'Kubernetes相关', link: '/devops/kubernetes/' },
       { text: '监控相关', link: '/devops/monitoring/' },
-      { text: '容器相关', link: '/devops/docker/' },
-      // 如需更多二级目录继续添加
     ]
   },
   {
     text: '编程',
     link: '/backend/',
     items: [
-      { text: 'go基础知识', link: '/02.backend/go-learn/' },
-      { text: 'go每日一库', link: '/02.backend/go-daily/' },
+      { text: 'go每日一库', link: '/pages/c232253e/' }, // go-daily
+      { text: 'go基础知识', link: '/pages/9a7ee40fc232253e/' }, //go-learn
       // 如需更多二级目录继续添加
     ]
   },
